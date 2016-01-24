e3727d8f71dbfd893d64dec53a8657c6430eff63
Marko <marko.ratkovic@fer.hr>
2016-01-24 11:05:35 +0100

    Updated cabal file, implemented methods for accessing review results

--- src/ReviewResults.hs
+++ src/ReviewResults.hs
@@ -37,20 +37,29 @@ databaseProviderResults action = do
 -- | Completes a review assignment and stores the result in a
 -- | file system or database.
 saveReview :: Review -> IO ()
-saveReview x = databaseProviderResults $ do 
-	insert x
-	return ()
+saveReview x = databaseProviderResults $ do
+    insert x
+    return ()
 
 -- | Loads all the completed review results for an assignment
 reviews :: Assignment -> IO [Review]
-reviews a = undefined
+reviews a = assignedReviews a >>= reviewsForAssigments
 
 -- | Loads all the completed review results for an assignment
 -- | that were performed by a user.
 reviewsBy :: Assignment -> UserIdentifier -> IO [Review]
-reviewsBy = undefined
+reviewsBy a uid = assignmentsBy a uid >>= reviewsForAssigments
+
 
 -- | Loads all the completed review results for an assignment
 -- | where the user’s code was being reviewed.
 reviewsFor :: Assignment -> UserIdentifier -> IO [Review]
-reviewsFor = undefined
+reviewsFor a uid = assignmentsFor a uid >>= reviewsForAssigments
+
+-- | Utility method that fetches reviews for assignments passed
+-- | as input parameter.
+reviewsForAssigments :: [ReviewAssignment] -> IO [Review]
+reviewsForAssigments as = databaseProviderResults $ do
+    reviews <- selectList [ReviewReviewAssignment /<-. as] []
+    liftIO . return $ map unwrapEntity reviews
+
